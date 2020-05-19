635b403bef83b41e301ac34d17755bb18293da42
Kenneth Jessen <kenn8174@elevcampus.dk>
2020-05-19 10:37:58 +0200

    Add project files.

---
+++ ADFSWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ADFSWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ADFSWeb.Areas.Identity.IdentityHostingStartup))]
+namespace ADFSWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ADFSWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ADFSWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ADFSWebContext>();
+            });
+        }
+    }
+}
